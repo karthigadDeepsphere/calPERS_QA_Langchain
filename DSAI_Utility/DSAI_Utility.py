1806bed0ab4023533ed1db677c481917f53a9b2f
Karthiga T <karthiga@deepsphere.ai>
2024-01-22 18:50:59 +0530

    added comments to each function

--- DSAI_Utility/DSAI_Utility.py
+++ DSAI_Utility/DSAI_Utility.py
@@ -8,30 +8,31 @@ def CSS_Property(file_name):
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 def All_Initialization():
+    '''
+    To make the selection in the application and exhibit the libraries in scope, and Model used.
+
+    return:
+        Return vAR_choice1 which is to select the type of application
+    '''
     col1,col2 = st.columns([5,5])
     with col1:
-        image = Image.open('DSAI_Utility/Logo_final.png')
+        image = Image.open('DSAI_Utility/Logo_final.png') 
         st.image(image)
     with col2:
         image = Image.open('DSAI_Utility/calPers_logo.png')
         st.image(image)
-    # st.markdown("<h1 style='text-align: center; color: #454545; font-size:25px;'>Generative AI empowers policy analysis for Employment Development</h1><h2 style='text-align: center; color: blue; font-size:20px;position: relative; top:-22px;'>With LLM, policy Q&A responses are easier to generate with citations</h2>", unsafe_allow_html=True)
-    # st.markdown("""
-    # <hr style="width:100%;height:3px;background-color:gray;border-width:10;position:relative;bottom:16px;">
-    # """, unsafe_allow_html=True)
     st.markdown("<h2 style='text-align: center; color: #454545; font-size:25px;'>Generative AI empowers policy analysis for Retirement System</h2><h2 style='text-align: center; color: blue; font-size:20px;position: relative; top:-30px;'>With LLM, policy Q&A responses are easier to generate</h2>", unsafe_allow_html=True)
-    # st.markdown("<h1 style='text-align: center; color: #454545; font-size:20px; top: '>Generative AI empowers policy analysis for Employment Developmen</h1>", unsafe_allow_html=True)
     st.markdown("""
     <hr style="width:100%;height:3px;background-color:gray;border-width:10;position:relative; bottom:30px;">
     """, unsafe_allow_html=True)
     st.sidebar.markdown("<h2 style='text-align: center; color: white; font-size:20px;'>Solution Scope</h2>", unsafe_allow_html=True)
     
-    choice2 =  st.sidebar.selectbox(" ",('Home','About Us'))
-    choice1 =  st.sidebar.selectbox(" ",('Select Application','Policy Analysis Q&A')) #,'Policy Analysis - Dialogflow','Policy Analysis - Evaluation'
-    choice3 =  st.sidebar.selectbox(" ",('Libraries in Scope','OpenAI','Streamlit'))
-    choice4 =  st.sidebar.selectbox(" ",('Models Used','ChatGPT', 'GPT3','GPT3 - Davinci','Llama','Titan'))
+    vAR_choice2 =  st.sidebar.selectbox(" ",('Home','About Us'))
+    vAR_choice1 =  st.sidebar.selectbox(" ",('Select Application','Policy Analysis Q&A')) #,'Policy Analysis - Dialogflow','Policy Analysis - Evaluation'
+    vAR_choice3 =  st.sidebar.selectbox(" ",('Libraries in Scope','OpenAI','Streamlit'))
+    vAR_choice4 =  st.sidebar.selectbox(" ",('Models Used','ChatGPT', 'GPT3','GPT3 - Davinci','Llama','Titan'))
     menu = ["Google Cloud Services in Scope","Cloud Storage", "Cloud Run", "Cloud Function", "Secret Manager","AWS-OpenSearch(Serverless VectorStore)"]
-    choice = st.sidebar.selectbox(" ",menu)
+    vAR_choice = st.sidebar.selectbox(" ",menu)
     st.sidebar.write('')
     st.sidebar.write('')
     href = """<form action="#">
@@ -40,7 +41,7 @@ def All_Initialization():
         padding: 7px;
         background-color: #32CD32;
         border: none;' type="submit" value="Clear/Reset" />
-</form>"""
+        </form>"""
     st.sidebar.markdown(href, unsafe_allow_html=True)
     st.sidebar.write('')
     st.sidebar.write('')
@@ -54,8 +55,4 @@ def All_Initialization():
         with col2:
             st.image('DSAI_Utility/aws_logo.png')
     
-    # vAR_clear_button = st.sidebar.button('Clear/Reset')
-    # if vAR_clear_button:
-    #     st.experimental_rerun()
-    
-    return choice1
+    return vAR_choice1
--- DSAI_source_code/DSAI_QALangchain.py
+++ DSAI_source_code/DSAI_QALangchain.py
@@ -1,30 +1,28 @@
 from langchain.document_loaders import PyPDFLoader
 from langchain.llms import OpenAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.chains import RetrievalQA
-from langchain.indexes import VectorstoreIndexCreator
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from streamlit_chat import message
-from langchain.prompts import ChatPromptTemplate,HumanMessagePromptTemplate,SystemMessagePromptTemplate
-
 import os
-
-
-
 import streamlit as st
 import time
 
-# os.environ["OPENAI_API_KEY"] = "" # Update with your API key
-os.environ["OPENAI_API_KEY"]
+os.environ["OPENAI_API_KEY"] = "" # Update with your API key
+# os.environ["OPENAI_API_KEY"]
 
 messages = []
 
 def input_fields():
+
+    '''
+    This function is to get the input file from the user. The file type here is pdf.
+
+    return:
+        This would return success message on uploading the correct formate of the file.
+    '''
         
     st.session_state.source_docs = st.file_uploader(label="LLM Knowledgebase Upload", type="pdf", accept_multiple_files=True)
     
@@ -32,19 +30,29 @@ def input_fields():
         st.success("Knowledge base file(s) successfully uploaded!")
 
 def process_documents():
+    '''
+    This function is to process the uploaded document. It would use the pdf loader to read the uploaded file and convert it to a list.
+    If there are multiple files, all the content in each files are append to the same list.
+
+    return: 
+        This function would store the list of words in documents to session_state.retriever
+    '''
     if st.session_state.source_docs:
         file_list = []
         documents=[]
         loader_pdf_list=[]
         
         for source_doc in st.session_state.source_docs:
+            # iterate each file to add to the list
             file_list.append("input_"+source_doc.name)
             with open("input_"+source_doc.name, mode='wb') as w:
                 w.write(source_doc.getvalue())
     
         for source_doc in file_list:
+            #iterate each file to load to pdfloader and extract the content
             loader_pdf = PyPDFLoader(source_doc)
             loader_pdf_list.append(loader_pdf)
+            #add the content to the list datatype documents
             documents.extend(loader_pdf.load())
             st.session_state.retriever_doc = documents
     else:
@@ -52,50 +60,71 @@ def process_documents():
         pass
         
 def conversational_retrieval_chain(retriever):
+    '''
+    This function split the text into chunks, store the text in chroma db, and retrive it. Also, works with conversational
+    retrival chain.
+
+    args:
+        retriever: list of text from document
+    
+    return:
+        Store the conversational retrieval chain object in st.session_state.qachain
+    '''
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     # texts=text_splitter.split_documents(documents)
     texts=text_splitter.split_documents(retriever)
+    
+    #create an open source embedding function
     embeddings=OpenAIEmbeddings()
-    db=Chroma.from_documents(texts, embeddings)
-    retriever=db.as_retriever(search_type="similarity", search_kwargs={"k":3})
-    qa = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(model_name="gpt-3.5-turbo",temperature=0),
+    
+    #load embeddings and texts to chroma
+    vAR_db=Chroma.from_documents(texts, embeddings)
+    #retrive from chroma using search_type similarity 
+    retriever=vAR_db.as_retriever(search_type="similarity", search_kwargs={"k":3})
+    #chain for having a conversation based on retrived documents. 
+    vAR_qa = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(model_name="gpt-3.5-turbo",temperature=0),
          retriever=retriever,
          return_source_documents=True)
     # qa = ConversationalRetrievalChain.from_llm(OpenAI(), retriever, return_source_documents=True)
-    st.session_state.qachain = qa
-    # return qa   
+    st.session_state.qachain = vAR_qa
     
 def query_llm(user_input):
-    vAR_qa = st.session_state.qachain
-    # qa = conversational_retrieval_chain(retriever)
-    # query="Can I add beneficiary to my retirement plan?"
-    query = user_input
-    result_queries=vAR_qa({"question":query, "chat_history":st.session_state.messages})
-    # print(result_queries["answer"])
-    st.session_state.messages.append((query, result_queries["answer"]))
-    # return result_queries["answer"]
-    return result_queries["answer"]+"\t CITATION : "+str(result_queries["source_documents"][0].metadata)
+    '''
+    This function queries to retrive the relevant answer to the query.
+
+    args:
+        user_input: Questions and follow up questions asked by the user
+
+    return:
+        Returns answer for the query along with cititation where it can be found in the uploaded documents
+    '''
+    vAR_qa_chain = st.session_state.qachain
+    vAR_query = user_input
+    #query also pass the chat history
+    vAR_result_queries=vAR_qa_chain({"question":vAR_query, "chat_history":st.session_state.messages})
+    #add the queries to the chat history
+    st.session_state.messages.append((vAR_query, vAR_result_queries["answer"]))
+    #return the answer for the query with citation
+    return vAR_result_queries["answer"]+"\t CITATION : "+str(vAR_result_queries["source_documents"][0].metadata)
     
 def QAwithLangchain():
+    '''
+    This is the main function to display the queries and their repective answers. This function calls processing document function, 
+    and other llm function to storing and retrive the answers.
+    '''
     start = time.time()
     start_time1 = time.time()
     input_fields()
     print('input file read - ',time.time()-start_time1)
     submit_button = False
     
-    
-    
-
-    
 
     if 'messages' not in st.session_state:
         st.session_state.messages = []
-
     if 'qachain' not in st.session_state:
         st.session_state.qachain = None
     if 'retriever' not in st.session_state:
-        st.session_state.retriever = None
-        
+        st.session_state.retriever = None        
     if 'source_docs' not in st.session_state:
         st.session_state.source_docs = None
     
@@ -108,9 +137,7 @@ def QAwithLangchain():
     
     if 'generated' not in st.session_state:
         if st.session_state.source_docs:
-            # st.session_state['generated'] = ["We provide customized information to our registered users on our website (www.edd.ca.gov)?"]
             st.session_state['generated'] = ["Ask your question here"]
-
 
     if 'past' not in st.session_state:
         if st.session_state.source_docs:
@@ -122,22 +149,16 @@ def QAwithLangchain():
     
     start_time3 = time.time()
     with container2:
-        # for message in st.session_state.messages:
-        #     st.chat_message('human').write(message[0])
-        #     st.chat_message('ai').write(message[1])    
-        # #
-        # if query := st.chat_input():
-        #     st.chat_message("human").write(query)
-        #     response = query_llm(st.session_state.retriever, query)
-        #     st.chat_message("ai").write(response)
         if st.session_state.source_docs:
             with st.form(key='my_form ', clear_on_submit=True):
                 if st.session_state.retriever_doc:
+                    #get the input from the user
                     user_input = st.text_input("Prompt:", placeholder="What information are you looking for?", key='user_input')
                     if st.session_state.qachain is None:
+                        #call the conversational retrival chain
                         conversational_retrieval_chain(st.session_state.retriever_doc)
+                    #pass the user input for querying the llm
                     response = query_llm(user_input)
-                    #qa_chain
 
                     # Add custom CSS for the button
                     st.write(
@@ -158,8 +179,9 @@ def QAwithLangchain():
                     submit_button = st.form_submit_button(label='Interact with LLM')
                     
                     if submit_button and user_input:
-                        
+                        #append the question
                         st.session_state['past'].append(user_input)
+                        #append the answer
                         st.session_state['generated'].append(str(response))
     print('query llm response - ',time.time()-start_time3)
     if 'generated' in st.session_state:         
@@ -168,11 +190,6 @@ def QAwithLangchain():
                 for i in range(len(st.session_state['generated'])):
                     message(st.session_state["past"][i],is_user=True, key=str(i) + '_user ', avatar_style="big-smile")
                     message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
-                # with st.chat_message("user"):
-                    #     st.write(st.session_state["past"][i])
-                    # with st.chat_message("assistant"):
-                    #     st.write(st.session_state["generated"][i])
-                    
     print('overall time - ',time.time()-start)      
     
     
